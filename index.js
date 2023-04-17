8c13317c0d11d3f56f27a394adb57a9208ba5f14
Scott Schwab <scottschwab86@gmail.com>
2023-04-17 14:02:40 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    })
+  })
+  .then((res) => res.json())
+  .then((data) => document.querySelector('body').append(data.id))
+  .catch((err) => document.querySelector('body').append(err))
+
+}
